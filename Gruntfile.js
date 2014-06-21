78b0be7836bfeebf5bd624e38da57a7b3596c219
Anderson Aguiar <revsoft10@gmail.com>
2014-06-20 22:14:14 -0300

    refactor(GruntFile): new tasks and refactor code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,134 +1,109 @@
 module.exports = function(grunt) {
 
+	'use strict';
+
+	// time of tasks
+	require('time-grunt')(grunt);
+	
 	// Load all tasks
-	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-
-	// Paths
-	var PathConfig = {
-		dev: 'dev/',
-		dist: 'dist/'
-	};
-
-	//Set scripts here for Uglify
-	var scripts = [
-		'<%= config.dev %>assets/_js/**/*.js'
-	]; 
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
 	grunt.initConfig({
-		//import package manifest - informations
-		pkg: grunt.file.readJSON("package.json"),
+		// import package manifest - informations
+		pkg: grunt.file.readJSON('package.json'),
 
-		//config path
-		config: PathConfig, 
+		// config
+		config: {
+			paths: {
+				assets: 	'assets/',
+	            sass:   	'sass/',
+	            css:    	'css/',
+	            js:     	'js/',
+	            img:   		'img/',
+	            fonts:  	'fonts/',
+		        ftp: 		'www',
+	            env: {
+	                dev: 	'app/',
+	                dist: 	'dist/'
+	            }
+			},
+			meta: {
+				banner: '/*\n' +
+				' *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+				' *  <%= pkg.description %>\n' +
+				' *  <%= pkg.url %>\n' +
+				' *\n' +
+				' *  Made by <%= pkg.author.name %>\n' +
+				' */\n'
+			}
+        },
 
-		//JShint
-		jshint: {
-			files: ['<%= config.dev %>assets/_js/**/*.js']
-		},
-
-		//clean files
+        // ## TASKS ##
+		// clean files
 		clean: {
 			dist: {
-				src: ["dist/"]
+				src: ['<%= config.paths.env.dist %>']
 			}
 		},
 
-		//copy files
+		// copy files
 		copy: {
 			dist: {
 				files: [
 					{
 						expand: true,
 						dot: true,
-						cwd: 'dev/',
+						cwd: '<%= config.paths.env.dev %>',
 						src: [
 							'**',
 							'*.{md,txt,htaccess}',
-							'!assets/**/.{png,jpg,gif,jpeg}',
-							'!assets/_**/**'
+							'!<%= config.paths.assets %>**/.{png,jpg,gif,jpeg}',
+							'!<%= config.paths.assets %>_**/**'
 						],
-						dest: 'dist/'
-					} // makes all src relative to cwd
+						dest: '<%= config.paths.env.dist %>'
+					}
 				]
 			}
 		}, 
 
-		//concat definitions
+		// concat definitions
 		concat: {
 			options: {
 				stripBanners: true,
-				banner: "<%= meta.banner %>"
+				banner: '<%= config.meta.banner %>'
 			},
 			js: {
-				src: ['<%= config.dev %>assets/js/**/*.js'],
-				dest: '<%= config.dist %>assets/js/all.min.js',
+				src: ['<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.js %>**/*.js'],
+				dest: '<%= config.paths.env.dist %><%= config.paths.assets %><%= config.paths.js %>all.min.js',
 			},
 			css: {
-				src: ['<%= config.dev %>assets/css/**/*.css'],
-				dest: '<%= config.dist %>assets/css/all.min.css',
+				src: ['<%= config.paths.env.dist %><%= config.paths.assets %><%= config.paths.css %>**/*.css'],
+				dest: '<%= config.paths.env.dist %><%= config.paths.assets %><%= config.paths.css %>all.min.css',
 			}
 		},
 
-		//banner definitions
-		meta: {
-			banner: "/*\n" +
-			" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
-			" *  <%= pkg.description %>\n" +
-			" *  <%= pkg.url %>\n" +
-			" *\n" +
-			" *  Made by <%= pkg.author.name %>\n" +
-			" */\n"
-		},
 
-		//uglify JS
-		uglify: {
-			options: {
-				mangle: false,
-			},
-			dist: {
-				files: {
-					'<%= config.dev %>/assets/js/scripts.min.js': scripts //stay here, because the copy of dist
-				}
-			},
-			dev: {
-				options: {
-					beautify: true
-				},
-				files: {
-					'<%= config.dev %>/assets/js/scripts.min.js': scripts
-				}
-			}
-		},
+		// uglify JS
+		// uglify: {
+		// 	options: {
+		// 		mangle: false,
+		// 	},
+		// 	dist: {
+		// 		files: {
+		// 			'<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.js %>scripts.min.js': scripts // stay here, because the copy of dist
+		// 		}
+		// 	},
+		// 	dev: {
+		// 		options: {
+		// 			beautify: true
+		// 		},
+		// 		files: {
+		// 			'<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.js %>scripts.min.js': scripts
+		// 		}
+		// 	}
+		// },
 
-		//minify HTML
-		htmlmin: {
-			dist: {
-				options: {
-					removeComments: true,
-					collapseWhitespace: true
-				},
-				files: [
-					{
-						expand: true,
-						cwd: '<%= config.dev %>/',
-						src: '*.html',
-						dest: '<%= config.dist %>/',
-					}
-				],
-			},
-			dev: {
-				files: [
-					{
-						expand: true,
-						cwd: '<%= config.dev %>/',
-						src: '*.html',
-						dest: '<%= config.dev %>/',
-					}
-				],
-			}
-		},
-
-		//minify images
+		// minify images
 		imagemin: {
 			dist: {
 				options: {
@@ -137,73 +112,65 @@ module.exports = function(grunt) {
 				files: [
 					{
 						expand: true,
-						cwd: '<%= config.dev %>/assets/img',
+						cwd: '<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.img %>',
 						src: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
-						dest: '<%= config.dist %>/assets/img',
+						dest: '<%= config.paths.env.dist %><%= config.paths.assets %><%= config.paths.img %>',
 					}
 				],
 			}
 		},
 
-		//css lint
+		// css lint
 		csslint: {
 			dev: {
 				csslintrc: '.csslintrc'
 			},
 			strict: {
-				src: ['<%= config.dev %>/assets/css/**/*']
+				src: ['<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.css %>**/*']
 			}
 		},
 
-		//compile scss
+		// compile scss
 		compass: {
 			dist: { 
 				options: { 
 					force: true,
 					config: 'config.rb',
-					sassDir: '<%= config.dev %>assets/_sass',
-					cssDir: '<%= config.dist %>assets/css',
+					sassDir: '<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.sass %>',
+					cssDir: '<%= config.paths.env.dist %><%= config.paths.assets %><%= config.paths.css %>',
 					outputStyle: 'compressed'
 				}
 			},
 			dev: { 
 				options: {
 					config: 'config.rb',
-					sassDir: '<%= config.dev %>assets/_sass',
-					cssDir: '<%= config.dev %>assets/css',
+					sassDir: '<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.sass %>',
+					cssDir: '<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.css %>',
 					outputStyle: 'nested',
 				}
 			}
 		},
 
-		//exec commands
+		// exec commands
 		exec: {
-			// Generate staging files (not optimized, but after preprocessing)
-			// In docpad, static is used for production ready build
-			// We use it as a pre-production build (staging), but
-			// still environment name for docpad is "static"
-			//
-			// TODO: replace this with docpad wrapper using docpad API
-			//
+		    cmd: 'npm install && bower install && grunt'
 		},
 
-		//FTP deployment
+		// FTP deployment
 		'ftp-deploy': {
 			build: {
 				auth: {
-					host: 'ftp.andersonaguiar.com', //your ftp host
+					host: 'ftp.xxx.xxx', // your ftp host
 					port: 21,
 					authKey: 'key1' //.ftppass file on the ./
 				},
-				src: './dist',
-				dest: 'grunt', //your remote directory(grunt was my test)
+				src: '<%= config.paths.env.dev %>',
+				dest: '<%= config.paths.ftp %>', // your remote directory
 				exclusions: [
-					'./**/.*', //all files what begin with dot
+					'./**/.*', // all files what begin with dot
 					'./**/Thumbs.db',
 					'./**/README.md',
-					'./**/*.zip',
-					// './**/node_modules',
-					// './**/dev'
+					'./**/*.zip'
 				]
 			}
 		},
@@ -212,47 +179,47 @@ module.exports = function(grunt) {
 		compress: {
 			all: {
 				options: {
-					archive: 'dist/all.zip'
+					archive: '<%= config.paths.env.dist %>all.zip'
 				},
 				files: [
 					{ 
-						/*flatten: true,*/ expand: true, cwd: './', src: ['./**'], dest: '' 
-					}, // includes files in path
+						expand: true, cwd: './', src: ['./**'], dest: '' 
+					},
 				]
 			},
 			dist: {
 				options: {
-					archive: 'dist/dist.zip'
+					archive: '<%= config.paths.env.dist %>dist.zip'
 				},
 				files: [
 					{ 
-						/*flatten: true,*/ expand: true, cwd: './', src: ['./dist/**'], dest: '' 
-					}, // includes files in path
+						expand: true, cwd: './', src: ['<%= config.paths.env.dist %>**'], dest: '' 
+					},
 				]
 			},
 			dev: {
 				options: {
-					archive: 'dist/dev.zip'
+					archive: '<%= config.paths.env.dist %>dev.zip'
 				},
 				files: [
 					{ 
-						/*flatten: true,*/ expand: true, cwd: './', src: ['./dev/**'], dest: '' 
-					}, // includes files in path
+						expand: true, cwd: './', src: ['<%= config.paths.env.dev %>**'], dest: '' 
+					},
 				]
 			}
 		},
 
-		//print screen (for this is necessary install phantomjs: http://phantomjs.org/download.html)
-		//if windows, set the path of phantom on Environment Variables
+		// print screen (for this is necessary install phantomjs: http:// phantomjs.org/download.html)
+		// if windows, set the path of phantom on Environment Variables
 		autoshot: {
 			default_options: {
 				options: {
-					path: './dev/screenshots',
+					path: '<%= config.paths.env.dev %>screenshots',
 					filename: 'screenshot',
 					type: 'jpg',
-					//remote: 'http://github.com/',
+					// remote: 'http://github.com/',
 					local: {
-						path: './dev',
+						path: '<%= config.paths.env.dev %>',
 						port: 7788
 					},
 					viewport: [
@@ -267,44 +234,63 @@ module.exports = function(grunt) {
 			},
 		},
 
-		//watcher project
+		//concurrent tasks
+		concurrent: {
+	        dev: [],
+	        // target2: ['jshint', 'mocha']
+	    },
+
+		// Keep multiple browsers & devices in sync when building websites.
+		browserSync: {
+		    dev: {
+		        bsFiles: {
+		            src : [
+		            	'<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.css %>**/*.css',
+		            	'<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.js %>**/*.js',
+		            	'<%= config.paths.env.dev %>**/*.html'
+		            ]
+		        },
+		        options: {
+                    watchTask: true,
+					server: {
+						baseDir: "app/"
+					},
+					// proxy: {
+					// 	host: "192.168.0.11",
+					// 	port: 8000
+					// }
+                }
+		    }
+		},
+
+		// watcher project
 		watch: {
 			options: {
-				debounceDelay: 500
+				// debounceDelay: 500
 			},
-			css: {
-				files: ['<%= config.dev %>assets/_sass/**/*'],
+			sass: {
+				files: ['<%= config.paths.env.dev %><%= config.paths.assets %><%= config.paths.sass %>**/*'],
 				tasks: ['compass:dev', 'csslint:strict']
 			},
 			js: {
 				files: [
-					'<%= config.dev %>**/js/*.js',
+					'<%= config.paths.env.dev %><%= config.paths.js %>**/*.js',
 				],
 				tasks: ['uglify:dev','jshint']
-			},
-			livereload: {
-				options: {
-					livereload: true
-				},
-				files: ['<%= config.dev %>assets/css/*.css', '<%= config.dev %>assets/js/scripts.js', '<%= config.dev %>**.html']
 			}
-		} // watch 
-
+		}
 	});
 
-	//dev
-	grunt.registerTask('dev', ['compass:dev']);
 
-	//deploy
+	// watch
+	grunt.registerTask('default', ['browserSync', 'watch']);
+
+	// build
+	grunt.registerTask('dist', ['clean', 'uglify:dist', 'copy:dist', 'concat:js', /*'concat:css',*/ 'compass:dist', 'jshint', 'imagemin:dist', 'htmlmin:dist'/*, 'zip'*/]);
+	
+	// deploy
 	grunt.registerTask('deploy', ['ftp-deploy:build']);
 
-	//compress
+	// compress
 	grunt.registerTask('zip', ['compress:dist','compress:dev','compress:all']);    
-
-	//build
-	grunt.registerTask('dist', ['clean', 'uglify:dist', 'copy:dist', 'concat:js', /*'concat:css',*/ 'compass:dist', 'jshint', 'imagemin:dist', 'htmlmin:dist'/*, 'zip'*/]);
-
-	//watch
-	grunt.registerTask('w', ['watch']);
-
 };
